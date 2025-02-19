a4e6080b30bb9abcfc4140bcc9b4ec562de23f6c
Saurabh Prakash <saurabhp85070@gmail.com>
2025-02-19 19:05:56 +0530

    changes added in admin.py of portfolio

--- myportfolio/portfolio/portfolio_app/admin.py
+++ myportfolio/portfolio/portfolio_app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Tech, Skill, Project, Certificate, CV
 
-# Register your models here.
-admin.site.register((Tech, Skill, Project, Certificate, CV))
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'display_priority')  # Display title and display_priority
+
+# Register other models normally
+admin.site.register((Tech, Skill, Certificate, CV))
