54dbe6850a902fda8c42ab93f5db13207747644b
Saurabh Prakash <saurabhp85070@gmail.com>
2025-04-07 20:15:10 +0530

    added comment in signals.py of userapp

--- Blog website Ullekh/ullekh/user_app/signals.py
+++ Blog website Ullekh/ullekh/user_app/signals.py
@@ -6,8 +6,9 @@ from .models import UserProfile
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        UserProfile.objects.create(user=instance) # UserProfile is the model class you created in models.py
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
+    # userprofile (lowercase) is the reverse relation automatically created by the OneToOneField in your UserProfile model.
